fad0702146574e62aa2f79fa9126e4105b2674d3
wsj7178 <wsj7178@gmail.com>
2017-12-19 00:08:41 +0900

    first release

---
+++ SafeJoin/SafeJoinMain.cs
@@ -0,0 +1,46 @@
+﻿using MiNET;
+using MiNET.Blocks;
+using MiNET.Plugins;
+using MiNET.Utils;
+using MiNET.Worlds;
+using System;
+
+namespace SafeJoin
+{
+    public class SafeJoinMain : Plugin
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            ConsoleColor bfColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[SafeJoin] 플러그인 활성화!");
+            Console.ForegroundColor = bfColor;
+            Context.Server.PlayerFactory.PlayerCreated += PlayerFactory_PlayerCreated;
+        }
+
+        private void PlayerFactory_PlayerCreated(object sender, MiNET.PlayerEventArgs e)
+        {
+            e.Player.PlayerJoin += Player_PlayerJoin;
+        }
+
+        private void Player_PlayerJoin(object sender, PlayerEventArgs e)
+        {
+            Level level = e.Level;
+            PlayerLocation location = e.Player.KnownPosition;
+            PlayerLocation safeLoc = GetSafeLocation(level, location);
+            e.Player.Teleport(safeLoc);
+            e.Player.SendMessage(ChatColors.Aqua + "[SafeJoin] 안전한 장소로 이동했습니다.", MiNET.MessageType.Raw);
+        }
+
+        private PlayerLocation GetSafeLocation(Level level, PlayerLocation location)
+        {
+            while (!(level.GetBlock(location) is Air))
+            {
+                location = location.Clone() as PlayerLocation;
+                location.Y++;
+            }
+            return location;
+        }
+    }
+}
